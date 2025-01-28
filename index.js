e40ace3e1772a98adac70a4170cc0a0b7185b4e5
mdferdausalam19 <mdferdausalam333@gmail.com>
2025-01-28 13:26:16 +0600

    Initial setup: Express.js server with basic structure and MongoDB configuration

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware setup for CORS and JSON parsing
+app.use(cors());
+app.use(express.json());
+
+// MongoDB dependencies and client initialization
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kmxsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a new MongoDB client with configuration
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log("Connected to MongoDB successfully!");
+  } catch (err) {
+    // Log any errors during connection or runtime
+    console.error("Error connecting to MongoDB: ", err.message);
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the CloudStay server! 🚀");
+});
+
+app.listen(port, () => {
+  console.log(`CloudStay server is running on port ${port}`);
+});
